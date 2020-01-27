d530182b6c337248cba635c7b6f432206792c2d1
P00159894 <dina.heidar@la.gov>
2020-01-27 14:38:40 -0600

    - making it work with .net core 3.1

--- SAML2Core.App/Controllers/HomeController.cs
+++ SAML2Core.App/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 
 namespace SAML2Core.App.Controllers
 {
+    //[AllowAnonymous]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
